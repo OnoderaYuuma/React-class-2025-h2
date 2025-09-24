c77cecb92fc46068effc5c66cba109b5d267a836
OnoderaYuuma <y.onodera.sys23@morijyobi.ac.jp>
2025-09-24 15:06:32 +0900

    コンポーネントの学習 09/24

---
+++ src/Mycomponent.js
@@ -0,0 +1,11 @@
+// export defaultを設定するとこのファイルに不空数関数があった場合でも
+// これを設定した関数が代表として出力される
+export default function Mycomponent() {
+  return (
+    <div>
+        <p>これはMycomponentから出力している文字列です</p>
+    </div>
+  )
+}
+
+// export default Mycomponent;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Mycomponent from './Mycomponent';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Mycomponent />
   </React.StrictMode>
 );
 
